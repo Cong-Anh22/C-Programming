ad0b8b955bfe022a92cbefcbd4c53f4341c14b5f
Cong-Anh22 <koivn22@gmail.com>
2026-09-16 11:20:56 +0900

    Update main.c

--- C/main.c
+++ C/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-
 int main() {
-    printf("Hello world!\n");
+    char name[50];
+    int ID;
+
+        printf("Hello World\n");
+
+        printf("My name is:");
+        scanf(" %49[^\n]", name);
+
+        printf("Student ID:");
+        scanf("%d", &ID);
     return 0;
 }
